d8ad50d41eae82d1f6896acfc6f2aa2f0d580d5b
Kevin <kevinlan321@gmail.com>
2020-06-29 17:23:40 -0700

    added map

---
+++ script.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2V2aW5qbDMyMSIsImEiOiJja2MxNHc2NzQxcTJ3MzFwM2xzbDZ1MWQ2In0.mDNPVHtcYZxBKYJ1njVYhA';
+    var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/kevinjl321/ckc15tzpx05u31jpde6rkpaga',
+    center: [12.550343, 55.665957],
+    zoom: 8
+});
+
+var marker = new mapboxgl.Marker().setLngLat([12.550343, 55.665957]).addTo(map);

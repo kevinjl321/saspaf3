f4a041d95fa0e488a65613048764f4a8a6885860
Kevin <kevinlan321@gmail.com>
2020-08-07 23:03:05 -0700

    finished get involved page

---
+++ src/main.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2V2aW5qbDMyMSIsImEiOiJja2MxNHc2NzQxcTJ3MzFwM2xzbDZ1MWQ2In0.mDNPVHtcYZxBKYJ1njVYhA';
+    var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});

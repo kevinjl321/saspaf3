889fc4a95b0aa0d0e157884d3bcb9b9afdea317b
Kevin <kevinlan321@gmail.com>
2020-08-16 12:25:10 -0700

    modified get involved page

--- src/components/get-involved/VolunteerInfo.vue
+++ src/components/get-involved/VolunteerInfo.vue
@@ -40,14 +40,8 @@
 </template>
 
 <script>
-    mapboxgl.accessToken = 'pk.eyJ1Ijoia2V2aW5qbDMyMSIsImEiOiJja2MxNHc2NzQxcTJ3MzFwM2xzbDZ1MWQ2In0.mDNPVHtcYZxBKYJ1njVYhA';
-    var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11'
-    });
-
     export default {
-        name: 'Volunteer-Info',
+        name: 'VolunteerInfo',
         components: {},
     }
 </script>
---
+++ src/views/GetInvolved.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="get-involved">
+    <home></home> 
+    <volunteer-info></volunteer-info>
+    <donate></donate>
+  </div>
+</template>
+
+<script>
+import VolunteerInfo from '@/components/get-involved/VolunteerInfo.vue'
+import Donate from '@/components/Donate.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    home: Home,
+    volunteerinfo: VolunteerInfo,
+    donate: Donate,
+  },
+}
+</script>
+
+<style scoped>
+
+
+</style>

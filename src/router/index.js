80bd46a1aea6b6bd6499a2f03ee7e41e323199bb
Kevin Lan <kevinlan321@gmail.com>
2020-09-05 22:53:28 -0700

    modified router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import GetInvolved from '../views/GetInvolved.vue'
 
 Vue.use(VueRouter)
 
@@ -13,10 +14,7 @@ Vue.use(VueRouter)
   {
     path: '/about',
     name: 'GetInvolved',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/GetInvolved.vue')
+    component: GetInvolved
   }
 ]
 
